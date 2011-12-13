255454750545a9b02b7a05d63336337b3f0d60c1
Kushagra <kushagr.singh3@gmail.com>
2011-12-13 23:27:57 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parivartann::Application.config.secret_token = '8220c0a266990fbd972993a4130f729e88c7bf7c4b94c53483290c80680524376a0efd72add2b783dc0ceb73c72beda05f75aa1b26edf328a7de3196294353a2'
