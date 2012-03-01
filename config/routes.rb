8f7dfcb671721201b66003b1631fbd3908eac476
Kushagra <kushagr.singh3@gmail.com>
2012-03-01 17:15:20 +0530

    More css added; images declared in html won't show up, think its some routing error

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Parivartann::Application.routes.draw do
 
   get "main/gallery"
 
+  map.resources "public"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -59,8 +61,8 @@ Parivartann::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
+  # oot"
+  # just remember to delete public/index.html.You can have the root of your site routed with "r
   # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
