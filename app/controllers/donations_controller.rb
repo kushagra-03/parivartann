259b6832b8a32ca503563612a78fc5356fc90f1a
Kushagra <kushagr.singh3@gmail.com>
2012-03-12 10:20:24 +0530

    reducing validations

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -6,7 +6,7 @@ class DonationsController < ApplicationController
   def create
     @donation = Donation.new(params[:donation])
     if @donation.save
-      redirect_to({:controller =>  :main ,:action =>  :home} , :notice => 'Your request has been saved. We will contact you soon' )
+      redirect_to(:action =>:home ,:notice => 'Your request has been saved. We will contact you soon', :controller => :main )
     else
       render :action => 'new'
     end
